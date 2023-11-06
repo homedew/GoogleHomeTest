f43222ac2a18c87abad32ce5cc9319ad05a6b109
xhomedew <homedew.tc@gmail.com>
2023-11-06 22:29:18 +0700

    TC
    TC

---
+++ Test1/Program.cs
@@ -0,0 +1,43 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+List<int?> tc1 = new List<int?> { 4, 5, 7, 12, 1, null, 8 };
+List<int?> tc2 = new List<int?> { 4, 5, 7, 8 ,12 , null, null, null};
+
+GenericTree tree1 = new GenericTree();
+Node root1 = tree1.NewNode(4);
+tree1.AddChild(root1, tree1.NewNode(5));
+tree1.AddChild(root1, tree1.NewNode(7));
+
+tree1.AddChild(root1.children[0], tree1.NewNode(12));
+tree1.AddChild(root1.children[0], tree1.NewNode(1));
+tree1.AddChild(root1.children[1], tree1.NewNode(8));
+
+
+var test1 = tree1.MostLeftRightSide(root1);
+Console.WriteLine("TC1");
+
+
+foreach (int number in test1)
+{
+    Console.Write(number + "\t");
+}
+
+
+GenericTree tree2 = new GenericTree();
+Node root2 = tree2.NewNode(4);
+tree2.AddChild(root2, tree2.NewNode(5));
+tree2.AddChild(root2, tree2.NewNode(7));
+tree2.AddChild(root2, tree2.NewNode(8));
+tree2.AddChild(root2.children[0], tree2.NewNode(12));
+
+
+Console.WriteLine("\nTC2");
+
+var test2 = tree2.MostLeftRightSide(root2);
+
+foreach (int number in test2)
+{
+    Console.Write(number + "\t");
+}
---
+++ Test1/TreeNode.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Node
+{
+    public int Val;
+    public List<Node> children;
+    public Node()
+    {
+
+    }
+    public Node(int val)
+    {
+        this.Val = val;
+        this.children = new List<Node>();
+    }
+
+}
+
+
+
+public class GenericTree
+
+{
+    
+    public Node NewNode(int val)
+    {
+        Node temp = new Node();
+        temp.Val = val;
+        temp.children = new List<Node>();
+        return temp;
+    }
+
+    public void AddChild(Node parent, Node child)
+    {
+        parent.children.Add(child);
+    }
+
+    public IList<int> MostLeftRightSide(Node root)
+    {
+        if (root == null) return new List<int>();
+
+        List<int> left = new List<int>();
+        List<int> right = new List<int>();
+
+        Queue<Node> queue = new Queue<Node>();
+        queue.Enqueue(root);
+        int currentLevel = 0;
+
+        while (queue.Count > 0)
+        {
+            int level = queue.Count;
+
+            for (int i = 0; i < level; i++)
+            {
+                Node node = queue.Dequeue();
+
+                if (i == 0)
+                {
+                    left.Add(node.Val);
+                }
+
+                if (i == level - 1 && i != 0)
+                {
+                    right.Add(node.Val);
+                }
+
+                if (node.children != null)
+                {
+                    foreach (Node child in node.children)
+                    {
+                        queue.Enqueue(child);
+                    }
+                }
+            }
+
+            currentLevel++;
+
+        }
+
+        if ( Math.Abs(left.Count - 1 - right.Count) > 0)
+        {
+            right.Add(left[currentLevel - 1]);
+        }
+
+        left.Reverse();
+        left.AddRange(right);
+
+        return left;
+    }
+
+}
+
+
